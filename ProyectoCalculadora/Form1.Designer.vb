fd7d588fbd2f1dd0f7568724f5dbda02ecef1992
alexandra <alexandramacedo96@gmail.com>
2024-10-31 14:09:22 +0100

    DAM2_UND1_ACT3

---
+++ ProyectoCalculadora/Form1.Designer.vb
@@ -0,0 +1,312 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class formPrincipal
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnPorcentaje = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btnComa = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btnC = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btnPotencia = New System.Windows.Forms.Button()
+        Me.btnIgual = New System.Windows.Forms.Button()
+        Me.btnSuma = New System.Windows.Forms.Button()
+        Me.btnResta = New System.Windows.Forms.Button()
+        Me.btnDiv = New System.Windows.Forms.Button()
+        Me.btnMulti = New System.Windows.Forms.Button()
+        Me.textDisplay = New System.Windows.Forms.TextBox()
+        Me.listHistorial = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btnPorcentaje
+        '
+        Me.btnPorcentaje.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPorcentaje.Location = New System.Drawing.Point(89, 156)
+        Me.btnPorcentaje.Name = "btnPorcentaje"
+        Me.btnPorcentaje.Size = New System.Drawing.Size(71, 62)
+        Me.btnPorcentaje.TabIndex = 0
+        Me.btnPorcentaje.Text = "%"
+        Me.btnPorcentaje.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn8.Location = New System.Drawing.Point(89, 219)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(71, 62)
+        Me.btn8.TabIndex = 1
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn5.Location = New System.Drawing.Point(89, 282)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(71, 62)
+        Me.btn5.TabIndex = 2
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn2.Location = New System.Drawing.Point(89, 345)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(71, 62)
+        Me.btn2.TabIndex = 3
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btnComa
+        '
+        Me.btnComa.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnComa.Location = New System.Drawing.Point(89, 408)
+        Me.btnComa.Name = "btnComa"
+        Me.btnComa.Size = New System.Drawing.Size(71, 62)
+        Me.btnComa.TabIndex = 4
+        Me.btnComa.Text = ","
+        Me.btnComa.UseVisualStyleBackColor = True
+        '
+        'btn0
+        '
+        Me.btn0.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn0.Location = New System.Drawing.Point(13, 408)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(71, 62)
+        Me.btn0.TabIndex = 9
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = True
+        '
+        'btn1
+        '
+        Me.btn1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn1.Location = New System.Drawing.Point(13, 345)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(71, 62)
+        Me.btn1.TabIndex = 8
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn4.Location = New System.Drawing.Point(13, 282)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(71, 62)
+        Me.btn4.TabIndex = 7
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btn7
+        '
+        Me.btn7.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn7.Location = New System.Drawing.Point(13, 219)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(71, 62)
+        Me.btn7.TabIndex = 6
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'btnC
+        '
+        Me.btnC.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnC.Location = New System.Drawing.Point(13, 156)
+        Me.btnC.Name = "btnC"
+        Me.btnC.Size = New System.Drawing.Size(71, 62)
+        Me.btnC.TabIndex = 5
+        Me.btnC.Text = "C"
+        Me.btnC.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn3.Location = New System.Drawing.Point(166, 345)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(71, 62)
+        Me.btn3.TabIndex = 13
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn6.Location = New System.Drawing.Point(166, 282)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(71, 62)
+        Me.btn6.TabIndex = 12
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn9.Location = New System.Drawing.Point(166, 219)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(71, 62)
+        Me.btn9.TabIndex = 11
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btnPotencia
+        '
+        Me.btnPotencia.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPotencia.Location = New System.Drawing.Point(166, 156)
+        Me.btnPotencia.Name = "btnPotencia"
+        Me.btnPotencia.Size = New System.Drawing.Size(71, 62)
+        Me.btnPotencia.TabIndex = 10
+        Me.btnPotencia.Text = "^"
+        Me.btnPotencia.UseVisualStyleBackColor = True
+        '
+        'btnIgual
+        '
+        Me.btnIgual.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnIgual.Location = New System.Drawing.Point(166, 408)
+        Me.btnIgual.Name = "btnIgual"
+        Me.btnIgual.Size = New System.Drawing.Size(148, 62)
+        Me.btnIgual.TabIndex = 19
+        Me.btnIgual.Text = "="
+        Me.btnIgual.UseVisualStyleBackColor = True
+        '
+        'btnSuma
+        '
+        Me.btnSuma.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSuma.Location = New System.Drawing.Point(243, 345)
+        Me.btnSuma.Name = "btnSuma"
+        Me.btnSuma.Size = New System.Drawing.Size(71, 62)
+        Me.btnSuma.TabIndex = 18
+        Me.btnSuma.Text = "+"
+        Me.btnSuma.UseVisualStyleBackColor = True
+        '
+        'btnResta
+        '
+        Me.btnResta.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnResta.Location = New System.Drawing.Point(243, 282)
+        Me.btnResta.Name = "btnResta"
+        Me.btnResta.Size = New System.Drawing.Size(71, 62)
+        Me.btnResta.TabIndex = 17
+        Me.btnResta.Text = "-"
+        Me.btnResta.UseVisualStyleBackColor = True
+        '
+        'btnDiv
+        '
+        Me.btnDiv.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDiv.Location = New System.Drawing.Point(243, 219)
+        Me.btnDiv.Name = "btnDiv"
+        Me.btnDiv.Size = New System.Drawing.Size(71, 62)
+        Me.btnDiv.TabIndex = 16
+        Me.btnDiv.Text = "/"
+        Me.btnDiv.UseVisualStyleBackColor = True
+        '
+        'btnMulti
+        '
+        Me.btnMulti.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMulti.Location = New System.Drawing.Point(243, 156)
+        Me.btnMulti.Name = "btnMulti"
+        Me.btnMulti.Size = New System.Drawing.Size(71, 62)
+        Me.btnMulti.TabIndex = 15
+        Me.btnMulti.Text = "*"
+        Me.btnMulti.UseVisualStyleBackColor = True
+        '
+        'textDisplay
+        '
+        Me.textDisplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.textDisplay.Location = New System.Drawing.Point(13, 95)
+        Me.textDisplay.Name = "textDisplay"
+        Me.textDisplay.Size = New System.Drawing.Size(301, 38)
+        Me.textDisplay.TabIndex = 20
+        Me.textDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'listHistorial
+        '
+        Me.listHistorial.FormattingEnabled = True
+        Me.listHistorial.Location = New System.Drawing.Point(13, 7)
+        Me.listHistorial.Name = "listHistorial"
+        Me.listHistorial.Size = New System.Drawing.Size(301, 82)
+        Me.listHistorial.TabIndex = 21
+        '
+        'formPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(329, 482)
+        Me.Controls.Add(Me.listHistorial)
+        Me.Controls.Add(Me.textDisplay)
+        Me.Controls.Add(Me.btnIgual)
+        Me.Controls.Add(Me.btnSuma)
+        Me.Controls.Add(Me.btnResta)
+        Me.Controls.Add(Me.btnDiv)
+        Me.Controls.Add(Me.btnMulti)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btnPotencia)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btnC)
+        Me.Controls.Add(Me.btnComa)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btnPorcentaje)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "formPrincipal"
+        Me.Text = "Calculadora"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnPorcentaje As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btnComa As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btnC As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnPotencia As Button
+    Friend WithEvents btnIgual As Button
+    Friend WithEvents btnSuma As Button
+    Friend WithEvents btnResta As Button
+    Friend WithEvents btnDiv As Button
+    Friend WithEvents btnMulti As Button
+    Friend WithEvents textDisplay As TextBox
+    Friend WithEvents listHistorial As ListBox
+End Class
---
+++ ProyectoCalculadora/Form1.vb
@@ -0,0 +1,121 @@
+﻿Public Class formPrincipal
+
+    ' Alexandra Leyli
+    ' Macedo Cotrina
+    ' DAM2
+    ' Desarrollo de interfaces
+    ' Actividad 03 - Calculadora
+
+    ' Declaración de variables globales para almacenar valores y la operación actual
+    Dim valor1 As Double    ' Primer valor ingresado
+    Dim valor2 As Double    ' Segundo valor ingresado
+    Dim operacion As String ' Tipo de operación seleccionada (+, -, *, /, ^, %)
+
+    ' Evento para ingresar números en el testDisplay
+    Private Sub btnNumero_Click(sender As Button, e As EventArgs) Handles btnComa.Click, btn9.Click, btn8.Click, btn7.Click, btn6.Click, btn5.Click, btn4.Click, btn3.Click, btn2.Click, btn1.Click, btn0.Click
+        ' Concatenar el texto del botón presionado al texto actual en el display
+        ' esta en la manera mas eficiente para hacer que parezcan los numeros en el display
+        textDisplay.Text += sender.Text
+    End Sub
+
+    ' Evento para limpiar el display y reiniciar valores y operación
+    Private Sub btnC_Click(sender As Object, e As EventArgs) Handles btnC.Click
+        ' Limpiar el display y reiniciar las variables 
+        textDisplay.Text = "" 'para reinicair el display, el texto debe de ser ""
+        valor1 = 0 'para reinicair el valor1 (variable global), debe de ser un 0
+        valor2 = 0 'para reinicair el valor2 (variable global), debe de ser un 0
+        operacion = "" 'para reinicair la operacion que es un string, el texto debe de ser ""
+    End Sub
+
+    ' Evento para calcular el porcentaje
+    Private Sub btnPorcentaje_Click(sender As Object, e As EventArgs) Handles btnPorcentaje.Click
+        ' Verificar si el display no está vacío y contiene un número válido
+        If textDisplay.Text <> "" AndAlso IsNumeric(textDisplay.Text) Then
+            valor1 = CDbl(textDisplay.Text)  ' Convertir el texto en número y asignarlo a valor1
+            operacion = "%"                 ' Asignar la operación de porcentaje
+            textDisplay.Text = (valor1 / 100).ToString()  ' Calcular el porcentaje (valor1 entre 100), convertirlo a String y mostrarlo en el display
+        Else
+            ' Mostrar un mensaje de error si el display está vacío o no contiene un número válido
+            MessageBox.Show("Por favor, ingresa un número antes de seleccionar la operación de porcentaje.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    ' Evento para seleccionar una operación aritmética (suma, resta, multiplicación, división)
+    Private Sub btnOperacion_Click(sender As Object, e As EventArgs) Handles btnSuma.Click, btnResta.Click, btnMulti.Click, btnDiv.Click
+        ' Verificar si el display no está vacío
+        If textDisplay.Text <> "" Then
+            valor1 = CDbl(textDisplay.Text)  ' Convertir el texto en número y asignarlo a valor1
+            operacion = sender.Text          ' Asignar la operación (el texto del botón presionado)
+            textDisplay.Text = ""            ' Limpiar/vaciar el display para el próximo valor
+        Else
+            ' Mostrar un mensaje de error si el display está vacío
+            MessageBox.Show("Por favor, ingresa un número antes de seleccionar una operación.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    ' Evento para seleccionar la operación de potencia
+    Private Sub btnPotencia_Click(sender As Object, e As EventArgs) Handles btnPotencia.Click
+        ' Verificar si el display no está vacío
+        If textDisplay.Text <> "" Then
+            valor1 = CDbl(textDisplay.Text)  ' Convertir el texto en número y asignarlo a valor1
+            operacion = "^"                  ' Asignar la operación de potencia
+            textDisplay.Text = ""            ' Limpiar el display para el próximo valor
+        Else
+            ' Mostrar un mensaje de error si el display está vacío
+            MessageBox.Show("Por favor, ingresa un número antes de seleccionar la operación de potencia.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    ' Evento para el botón igual, realiza la operación seleccionada y muestra el resultado
+    Private Sub btnIgual_Click(sender As Object, e As EventArgs) Handles btnIgual.Click
+        ' Verificar que el display no esté vacío y contenga un número válido
+        If textDisplay.Text <> "" AndAlso IsNumeric(textDisplay.Text) Then
+            Dim resultado As Double 'Variable de duble para almacenar el resultado
+
+            ' Si la operación es porcentaje, se calcula directamente y no se requiere un segundo valor
+            If operacion = "%" Then
+                resultado = valor1 / 100  ' Calcular el porcentaje
+                listHistorial.Items.Add($"{valor1} % = {resultado}") ' Guardar el cálculo en el historial
+                textDisplay.Text = resultado.ToString()              ' Mostrar el resultado en el display
+                operacion = ""                                       ' Limpiar la operación
+                Exit Sub                                             ' Terminar el procedimiento aquí
+            End If
+
+            valor2 = CDbl(textDisplay.Text)  ' Convertir el texto del display en número y asignarlo a valor2
+
+            ' Seleccionar la operación aritmética según el valor de la variable "operacion"
+            Select Case operacion
+                Case "+"
+                    resultado = valor1 + valor2  ' Sumar valor1 y valor2
+                Case "-"
+                    resultado = valor1 - valor2  ' Restar valor2 de valor1
+                Case "*"
+                    resultado = valor1 * valor2  ' Multiplicar valor1 y valor2
+                Case "/"
+                    If valor2 <> 0 Then          ' Verificar que valor2 no sea 0
+                        resultado = valor1 / valor2  ' Dividir valor1 por valor2
+                    Else
+                        textDisplay.Text = "Error"  ' Mostrar error si valor2 es 0
+                        Exit Sub
+                    End If
+                Case "^"
+                    resultado = Math.Pow(valor1, valor2)  ' Elevar valor1 a la potencia de valor2
+                Case Else
+                    textDisplay.Text = "Error"            ' Mostrar error si la operación es desconocida
+                    Exit Sub
+            End Select
+
+            ' Mostrar el resultado (que se convierte en String) de la operación en el display
+            textDisplay.Text = resultado.ToString()
+
+            ' Agregar el cálculo al historial en formato legible
+            listHistorial.Items.Add($"{valor1} {operacion} {valor2} = {resultado}")  ' aparecera el Valor1 / la operacion / valor2 / = / resultado(variable creada al inicio)
+        Else
+            ' Mostrar mensaje de error si el display está vacío o no tiene un número válido
+            MessageBox.Show("Por favor, ingresa un número válido antes de presionar el botón Igual.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+End Class
+
+
+
